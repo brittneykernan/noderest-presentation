f111053761c843e4d26ffb23a9a799a6b5c7879d
Brittney Kernan <b.kernan@bigspaceship.com>
2014-04-28 00:42:05 -0400

    added terminal

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,34 +9,101 @@ var deck = bespoke.from('article', {
   forms: true
 });
 
-var off = deck.on('activate', function(e) {
+if(document.body.style.opacity == 0) {
+   setTimeout(function() {
+      document.body.style.opacity = 1
+    },650);
+}
+
+var terminal = document.getElementById('terminal');
+var lis = terminal.getElementsByTagName('li');
+var liIndx = 0;
+
+deck.on('activate', function(e) {
   // fade in
-  if(document.body.style.opacity == 0)
-     setTimeout(function() {
-        document.body.style.opacity = 1
-      },650);
 
-  console.log(e.index)
-
-  var bodyClass = '';
-  if( e.index < 5 ) 
-    bodyClass = '';
-  else if( e.index < 8 ) {
-    bodyClass = 'folders-open-1' 
-  } else if( e.index < 10 ) {
-    bodyClass = 'folders-open-2' 
-  } else if ( e.index < 16 ){
-    bodyClass = 'folders-open-3'
-  } else if ( e.index < 17 ) {
-    bodyClass = 'folders-open-4'    
-  } else {
-    bodyClass = 'folders-open-5' 
-  } 
-
-  document.body.className = bodyClass
-  
+  updateFolder(e);
+  console.log('cur index',e.index)
+  updateTerminal(e)
   
 });
+
+function updateFolder(e) {
+  var step = 0, term = 0;
+  if( e.index < 4 ) 
+    step = 0;
+  else if( e.index < 8 ) {
+    step = 1; 
+  } else if( e.index < 10 ) {
+    step = 2 
+  } else if ( e.index < 16 ){
+    step = 3
+  } else if ( e.index < 17 ) {
+    step = 4   
+  } else {
+    step = 5
+  } 
+
+  if(step > 0)
+  document.body.className = 'folders-open-' + step 
+}
+
+function updateTerminal(e) {
+
+  if( e.index > 4 && lis[1].style.display != 'block') {
+    terminal.className = 'show-term'
+
+    showNextLi()
+    setTimeout(function() {
+      showNextLi()
+    }, 500);
+  }
+
+  if( e.index > 5 && lis[2].style.display != 'block') {
+    showNextLi()
+  }
+
+  if( e.index > 8 && lis[3].style.display != 'block') {
+    showNextLi()
+  }
+
+  if( e.index > 9 && lis[6].style.display != 'block') {
+    var timing = (e.index == 10) ? 250: 0;
+
+    setTimeout(function() {
+      showNextLi()
+      window.term = setInterval(function() {
+        showNextLi()
+        if( liIndx > 19 ) 
+          window.clearInterval(window.term)
+     }, timing);
+    }, 1000)
+  }
+
+  if( e.index > 11 && lis[20].style.display != 'block') {
+    showNextLi();
+    setTimeout(function() {
+      showNextLi()
+    }, 500)
+  }
+
+  if( e.index > 18 && lis[22].style.display != 'block') {
+    showNextLi();
+    setTimeout(function() {
+      showNextLi()
+      setTimeout(function() {
+        showNextLi()
+      }, 500)
+    }, 500)
+  }
+}
+
+function showNextLi() {
+  console.log('showing',liIndx)
+  lis[liIndx].style.display = 'block';
+  liIndx ++ 
+  terminal.scrollTop = terminal.scrollHeight;
+}
 
 
 try{Typekit.load();}catch(e){}      
